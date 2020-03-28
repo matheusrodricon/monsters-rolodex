645ec505cff812987e93e10fadd0a0ab315a0ad9
Matheus Rodrigues Conceição <matheusrodri@gmail.com>
2020-03-27 22:44:07 -0300

    Updates

---
+++ precache-manifest.ad0a898ea5810fb6f549611100ec66a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9cac65d6b85aa6cd72de8cd0d6e967",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ae9b21ee42f2a2f3fe48",
+    "url": "/monsters-rolodex/static/css/main.2bff09cd.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9b21ee42f2a2f3fe48",
+    "url": "/monsters-rolodex/static/js/main.cde33477.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
